5d0715f2d711114cf6ca1513fc898863c0c1ccec
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-02 20:36:42 +0300

    update

--- plugin/kamailio.lua
+++ plugin/kamailio.lua
@@ -1,32 +1,32 @@
 --return {}
-vim.filetype.add({
-	extension = {
-		bbbcfg = "kamailio",
-	},
-	filename = {
-		["kamctlrc"] = "kamailio",
-	},
-	pattern = {
-		[".*"] = {
-			--function(path, bufnr)
-			function(_, bufnr)
-				local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ""
-				if vim.regex([[^#!.*KAMAILIO]]):match_str(content) ~= nil then
-					return "kamailio"
-				end
-			end,
-		},
-	},
-})
+vim.filetype.add {
+  extension = {
+    cfg = 'kamailio',
+  },
+  filename = {
+    ['kamctlrc'] = 'kamailio',
+  },
+  pattern = {
+    ['.*'] = {
+      --function(path, bufnr)
+      function(_, bufnr)
+        local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+        if vim.regex([[^#!.*KAMAILIO]]):match_str(content) ~= nil then
+          return 'kamailio'
+        end
+      end,
+    },
+  },
+}
 
-vim.api.nvim_create_autocmd("FileType", {
-	pattern = "kamailio",
-	--callback = function(args)
-	callback = function()
-		-- vim.lsp.buf_attach_client(args.buf, client)
-		local root_dir = vim.fn.getcwd()
-		if root_dir then
-			require("kamailio").start("kamailio")
-		end
-	end,
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'kamailio',
+  --callback = function(args)
+  callback = function()
+    -- vim.lsp.buf_attach_client(args.buf, client)
+    -- local root_dir = vim.fn.getcwd()
+    -- if root_dir then
+    require('kamailio').start 'kamailio'
+    -- end
+  end,
 })

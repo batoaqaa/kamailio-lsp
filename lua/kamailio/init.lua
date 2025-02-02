61e0ae386147758980c4d7a334b54b5d43783eeb
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-02 18:56:39 +0300

    update

---
+++ lua/kamailio/init.lua
@@ -0,0 +1,54 @@
+local M = {}
+local state = { autocmd = {} }
+
+-- local augroup = vim.api.nvim_create_augroup('client_cmds', { clear = true })
+-- local autocmd = vim.api.nvim_create_autocmd
+local fmt = string.format
+
+M.start = function(name)
+	local config = M.config(name)
+	local id = vim.lsp.start(config.params)
+	if not id then
+		return
+	end
+
+	-- if vim.v.vim_did_enter == 1 then
+	--   M.buf_attach(config, id)
+	-- end
+	--
+	-- state.autocmd[id] = autocmd('BufEnter', {
+	--   pattern = '*',
+	--   group = augroup,
+	--   desc = fmt('Attach LSP: %s', name),
+	--   callback = function()
+	--     M.buf_attach(config, id)
+	--   end,
+	-- })
+end
+
+--M.config = function(name, opts)
+M.config = function(name, _)
+	local server_opts = require(fmt("%s.client", name))
+	server_opts.params.on_exit = M.on_exit
+
+	return server_opts
+end
+
+-- M.buf_attach = function(config, id)
+--   local supported = config.filetypes[vim.bo.filetype]
+--   if not supported then
+--     return
+--   end
+--
+--   local bufnr = vim.api.nvim_get_current_buf()
+--   vim.lsp.buf_attach_client(bufnr, id)
+-- end
+
+--M.on_exit = function(code, signal, client_id)
+M.on_exit = function(_, _, client_id)
+	vim.schedule(function()
+		vim.api.nvim_del_autocmd(state.autocmd[client_id])
+	end)
+end
+
+return M

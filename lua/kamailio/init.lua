a917a8c4c0eac5ac7cb9b841f014e3fab405ae46
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-14 23:09:29 +0300

    Update

--- lua/kamailio/init.lua
+++ lua/kamailio/init.lua
@@ -48,7 +48,7 @@ if not parser_config['kamailio'] then
       -- url = 'https://github.com/IbrahimShahzad/tree-sitter-kamailio-cfg',
       -- url = 'https://github.com/batoaqaa/tree-sitter-kamailio',
       -- url = '~/tree-sitter-kamailio-cfg',
-      url = '~/my',
+      url = '~/tree-sitter-kamailio',
 
       files = { 'src/parser.c' }, -- note that some parsers also require src/scanner.c or src/scanner.cc
       -- optional entries:

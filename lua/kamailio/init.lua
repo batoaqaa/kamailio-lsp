eb197fa9f3852fee26e94f29b23354b4f5fbff35
batoaqaa <ahmed.batoaq@gmail.com>
2025-02-15 06:56:36 +0300

    Update

--- lua/kamailio/init.lua
+++ lua/kamailio/init.lua
@@ -64,12 +64,12 @@ end
 local ensure_installed = require('nvim-treesitter.configs').get_ensure_installed_parsers()
 -- add to ensure_installed table if valid
 if type(ensure_installed) == 'table' then
-  ensure_installed[#ensure_installed + 1] = 'kamailio'
+  ensure_installed[#ensure_installed + 1] = 'kamailio_cfg'
   local opts = { ensure_installed = ensure_installed }
   require('nvim-treesitter.configs').setup(opts)
 -- else install parser
-elseif parser_config['kamailio'] and not parsers.has_parser 'kamailio' then
-  vim.cmd 'TSInstallSync kamailio'
+elseif parser_config['kamailio_cfg'] and not parsers.has_parser 'kamailio_cfg' then
+  vim.cmd 'TSInstallSync kamailio_cfg'
 end
 ---------------------------------------------------------------------------------------------------------------
 
